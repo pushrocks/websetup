5df46f8b7717c02bbfefbdf97ee4f763adb24147
Phil Kunz <gitlab@philkunz.com>
2020-07-08 11:10:20 +0000

    fix(core): update

---
+++ test/test.browser.ts
@@ -0,0 +1,16 @@
+import { expect, tap } from '@pushrocks/tapbundle';
+import * as websetup from '../ts/index';
+
+tap.test('first test', async () => {
+  const websetupInstance = new websetup.WebSetup({
+    metaObject: {
+      description: 'A awesome description',
+      title: 'mytitle',
+      canonicalDomain: 'lossless.com'
+    }
+  });
+  await websetupInstance.setup();
+  expect(document.title).to.equal('mytitle');
+});
+
+tap.start();
--- test/test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, tap } from '@pushrocks/tapbundle';
-import * as websetup from '../ts/index';
-
-tap.test('first test', async () => {
-  console.log('Waiting for proper puppeteer support here');
-});
-
-tap.start();
